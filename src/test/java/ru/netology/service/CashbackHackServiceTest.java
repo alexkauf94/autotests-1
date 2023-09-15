0d9cb1b00a72ef6cb3a006f8884b07767a3c2149
Alex Kaufman <alexkauf94@gmail.com>
2023-09-15 16:35:54 +0700

    Added testng tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,39 +1,46 @@
-import org.testng.annotations.Test;
-import ru.netology.service.CashbackHackService;
+package ru.netology.service;
 
-import static org.testng.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService cashbackHackService = new CashbackHackService();
 
     @Test
-    public void remainShouldCalculatedAmountZeroAmount() {
+    public void remainShouldCalculateZeroAmount() {
         int amount = 0;
-
         int actual = cashbackHackService.remain(amount);
         int expected = 1000;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void remainShouldCalculatedAmountUnderBoundary() {
-        int amount = 9999;
-
+    public void remainShouldCalculateZeroAmountUnderBoundary() {
+        int amount = 1999;
         int actual = cashbackHackService.remain(amount);
         int expected = 1;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void remainShouldCalculatedAmountOverBoundary() {
-        int amount = 10001;
-
+    public void remainShouldCalculateZeroAmountOverBoundary() {
+        int amount = 10_001;
         int actual = cashbackHackService.remain(amount);
         int expected = 999;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainShouldCalculateZeroAmountEqualsBoundary() {
+        int amount = 8000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
     }
 }
